d636d786518527b402dc0e89ed96dfc74d077a84
PhamNhatHuy <huy.phamnhat@asnet.com.vn>
2023-10-26 16:56:13 +0700

    test: update writing AddCard component unit test

--- src/components/common/Cards/AddCard/__test__/AddCard.test.tsx
+++ src/components/common/Cards/AddCard/__test__/AddCard.test.tsx
@@ -3,13 +3,13 @@ import userEvent from '@testing-library/user-event';
 
 import { AddCard } from '..';
 
-describe('AddCard test case', () => {
+describe('AddCard test cases', () => {
   it('should render correctly with onclick function', () => {
     const mockOnClick = vi.fn();
 
     const mockAddCard = customRenderer(<AddCard onClick={mockOnClick} />);
 
-    expect(mockAddCard.toJSON());
+    expect(mockAddCard.toJSON()).toMatchSnapshot();
   });
 
   it('should invoke onclick function when being clicked', async () => {
