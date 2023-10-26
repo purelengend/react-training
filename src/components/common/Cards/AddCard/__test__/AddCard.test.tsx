2c30306472fc980aada573eaf9438c5c32fc081b
PhamNhatHuy <huy.phamnhat@asnet.com.vn>
2023-10-26 16:50:23 +0700

    test: finish writing AddCard component unit test

---
+++ src/components/common/Cards/AddCard/__test__/AddCard.test.tsx
@@ -0,0 +1,26 @@
+import { customRenderer, render } from '@src/test/test-utils';
+import userEvent from '@testing-library/user-event';
+
+import { AddCard } from '..';
+
+describe('AddCard test case', () => {
+  it('should render correctly with onclick function', () => {
+    const mockOnClick = vi.fn();
+
+    const mockAddCard = customRenderer(<AddCard onClick={mockOnClick} />);
+
+    expect(mockAddCard.toJSON());
+  });
+
+  it('should invoke onclick function when being clicked', async () => {
+    const mockOnClick = vi.fn();
+
+    const { container } = render(<AddCard onClick={mockOnClick} />);
+
+    const mockAddCard = container.querySelector('#add-card')!;
+
+    await userEvent.click(mockAddCard);
+
+    expect(mockOnClick).toBeCalled();
+  });
+});
