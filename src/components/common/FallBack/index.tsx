e6571b32d8fcf79422fbdc4005f7b565dc21d967
PhamNhatHuy <huy.phamnhat@asnet.com.vn>
2023-11-02 10:45:21 +0700

    fix: fix capitalized Fallback folder name on remote repo

--- src/components/common/FallBack/index.tsx
+++
@@ -1,9 +0,0 @@
-import fallbackStyles from '@components/common/FallBack/Fallback.module.css';
-
-export const Fallback = () => {
-  return (
-    <div className={`d-flex-center ${fallbackStyles['fallback-message']}`}>
-      Something went wrong
-    </div>
-  );
-};
